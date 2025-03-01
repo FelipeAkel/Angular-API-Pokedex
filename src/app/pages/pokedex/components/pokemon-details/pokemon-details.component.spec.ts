0cf744cbb75d7c60952d18b1efd6933ff6cd5d76
Felipe Akel <95441539+FelipeAkel@users.noreply.github.com>
2025-02-28 22:56:12 -0300

    TEST: PokemonDetailsComponent

--- src/app/pages/pokedex/components/pokemon-details/pokemon-details.component.spec.ts
+++ src/app/pages/pokedex/components/pokemon-details/pokemon-details.component.spec.ts
@@ -1,14 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PokemonDetailsComponent } from './pokemon-details.component';
+import { DynamicDialogConfig, DynamicDialogRef } from 'primeng/dynamicdialog';
+import { PokedexApiService } from '../../../../services/pokedex-api.service';
+
+import { HttpClientTestingModule } from '@angular/common/http/testing';  // Importando o módulo para simular requisições HTTP
+
+class MockDynamicDialogConfig {
+  data = { id: 1 };  // Simulando que o dialog está recebendo o ID 1
+}
 
 describe('PokemonDetailsComponent', () => {
   let component: PokemonDetailsComponent;
   let fixture: ComponentFixture<PokemonDetailsComponent>;
+  let dynamicDialogRefSpy: jasmine.SpyObj<DynamicDialogRef>;
 
   beforeEach(async () => {
+
+    dynamicDialogRefSpy = jasmine.createSpyObj('DynamicDialogRef', ['close']);
+
     await TestBed.configureTestingModule({
-      imports: [PokemonDetailsComponent]
+      imports: [
+        PokemonDetailsComponent,
+        HttpClientTestingModule  // Importando o módulo para simular o HttpClient
+      ],
+      providers: [
+        { provide: DynamicDialogRef, useValue: dynamicDialogRefSpy },        // Provedor para o DynamicDialogRef
+        { provide: DynamicDialogConfig, useClass: MockDynamicDialogConfig },  // Provedor para o DynamicDialogConfig
+        PokedexApiService
+      ],
+    
     })
     .compileComponents();
 
@@ -17,7 +38,61 @@ describe('PokemonDetailsComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('should create', () => { 
     expect(component).toBeTruthy();
   });
+
+  it(`(U) ao executar formattedTypes(), deveria pegar os valores do array e retornar uma string separado por virgula`, () => {
+    const mockItem = {
+      types: [
+        {
+          slot: 1,
+          type: {
+            name: "grass",
+          }
+        },
+        {
+          slot: 2,
+          type: {
+            name: "poison",
+          }
+        },
+        {
+          slot: 3,
+          type: {
+            name: "normal",
+          }
+        }
+      ]
+    };
+
+    component.formattedTypes(mockItem);
+
+    expect(component.formattedTypes(mockItem)).toBe('Grass, Poison, Normal');
+  });
+
+  it(`(U) ao executar formattedTypes(), deveria caso array vazio retornar vazio`, () => {
+    const mockItem = {
+      types: [
+        {
+          slot: 1,
+          type: {
+            name: "",
+          }
+        },
+      ]
+    };
+
+    component.formattedTypes(mockItem);
+
+    expect(component.formattedTypes(mockItem)).toBe('');
+  })
+  
+  it(`(U) ao executar closeDialog(), deveria fechar o DynamicDialogRef`, () => {
+    
+    component.closeDialog();
+
+    expect(dynamicDialogRefSpy.close).toHaveBeenCalled();
+  });
+
 });
