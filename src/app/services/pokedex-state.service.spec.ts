92cc86f9a652c79da19abdddca2130c6ba1c2c39
Felipe Akel <95441539+FelipeAkel@users.noreply.github.com>
2025-03-03 10:05:40 -0300

    TEST: PokedexStateService

--- src/app/services/pokedex-state.service.spec.ts
+++ src/app/services/pokedex-state.service.spec.ts
@@ -2,6 +2,15 @@ import { TestBed } from '@angular/core/testing';
 
 import { PokedexStateService } from './pokedex-state.service';
 
+const mockPokedexFormFilter = {
+  inNamePokemon: 'pikachu',
+  inTypePokemon: {
+      value: 'electric', 
+      name: 'Elétrico - Electric', 
+      class: 'electric',
+  }
+}
+
 describe('PokedexStateService', () => {
   let service: PokedexStateService;
 
@@ -13,4 +22,22 @@ describe('PokedexStateService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it(`(U) ao executar setPokedexFormFilterState(), deveria atribuir o valores do filtro a uma variavel`, () => {
+    spyOn(service['pokedexFormFilterState'], 'next');
+    
+    service.setPokedexFormFilterState(mockPokedexFormFilter);
+
+    expect(service['pokedexFormFilterState'].next).toHaveBeenCalledWith(mockPokedexFormFilter);
+  });
+
+  it(`(U) ao executar resetPokedexFormFilterState(), deveria apagar os valores contidos na variavel`, () => {
+    spyOn(service['pokedexFormFilterState'], 'next');
+    
+    service['pokedexFormFilterState'].next(mockPokedexFormFilter);
+    service.resetPokedexFormFilterState();
+
+    expect(service['pokedexFormFilterState'].next).toHaveBeenCalledWith({});
+  });
+
 });
