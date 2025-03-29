d6bb8301a1b93ea7e1e839a39f4a6891d7d8d5d2
Felipe Akel <95441539+FelipeAkel@users.noreply.github.com>
2025-03-28 22:55:18 -0300

    FEAT: Adicionando novos campos ao formulário de cadastro todo-list

--- src/app/pages/todo-list/component/form-create-edit-task/form-create-edit-task.component.ts
+++ src/app/pages/todo-list/component/form-create-edit-task/form-create-edit-task.component.ts
@@ -1,18 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Message } from 'primeng/message';
 import { DatePickerModule } from 'primeng/datepicker';
-import { FormsModule } from '@angular/forms';
 import { FluidModule } from 'primeng/fluid';
 import { ButtonModule } from 'primeng/button';
+import { RadioButton } from 'primeng/radiobutton';
 
 @Component({
   selector: 'app-form-create-edit-task',
-  imports: [Message, DatePickerModule, FormsModule, FluidModule, ButtonModule],
+  imports: [Message, DatePickerModule, FormsModule, FluidModule, ButtonModule, RadioButton],
   templateUrl: './form-create-edit-task.component.html',
   styleUrl: './form-create-edit-task.component.scss'
 })
 export class FormCreateEditTaskComponent {
 
-  date2: Date | undefined;
+  public checked: boolean = false;
+  public dtExpiration: Date | undefined;
 
 }
