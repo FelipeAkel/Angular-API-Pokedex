f8172da844074670552cfaaddb3a8ddc759b62df
Felipe Akel <95441539+FelipeAkel@users.noreply.github.com>
2025-04-17 18:28:21 -0300

    TEST: Component ArchitectureDesignComponent 100% de cobertura

--- src/app/pages/architecture-design/architecture-design.component.spec.ts
+++ src/app/pages/architecture-design/architecture-design.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ArchitectureDesignComponent } from './architecture-design.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+import { mockArchitectureDesign } from '../../mocks/architecture-design.mock';
 
 describe('ArchitectureDesignComponent', () => {
   let component: ArchitectureDesignComponent;
@@ -8,7 +11,10 @@ describe('ArchitectureDesignComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ArchitectureDesignComponent]
+      imports: [
+        ArchitectureDesignComponent,
+        HttpClientTestingModule
+      ]
     })
     .compileComponents();
 
@@ -20,4 +26,35 @@ describe('ArchitectureDesignComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+  
+  it(`(U) ao executar ngOnInit(), deveria executar a getListArchitecture`, () => {
+    spyOnProperty(component['architectureService'], 'getListArchitecture', 'get').and.returnValue(
+      of(mockArchitectureDesign)
+    );
+    
+    component.ngOnInit();
+    
+    expect(component.dataArchitectureDesign).toEqual(mockArchitectureDesign);
+  });
+  
+  it(`(U) ao executar getIconClass(), deveria retornar o nome da class`, () => {
+    const spinnerDotted = component.getIconClass('pi pi-spin pi-spinner-dotted');
+    const fileCheck = component.getIconClass('pi pi-fw pi-file-check');
+    const file = component.getIconClass('pi pi-fw pi-file');
+    const code = component.getIconClass('pi pi-fw pi-code');
+    const sparkles = component.getIconClass('pi pi-fw pi-sparkles');
+    const folder = component.getIconClass('pi pi-fw pi-folder');
+    const folderOpen = component.getIconClass('pi pi-fw pi-folder-open');
+    const returnDefault = component.getIconClass('pi pi-fw pi-times');
+
+    expect(spinnerDotted).toEqual('json-yellon');
+    expect(fileCheck).toEqual('file-check-orange');
+    expect(file).toEqual('file-blue');
+    expect(code).toEqual('code-orange');
+    expect(sparkles).toEqual('sparkles-pink');
+    expect(folder).toEqual('folder-gray');
+    expect(folderOpen).toEqual('folder-open-primary-color');
+    expect(returnDefault).toEqual('icon-default');
+  });
+
 });
